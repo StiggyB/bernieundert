f6457cbc2d622a44eafbe3c4e66c8ec6de8fdb83
sven.p.schliesing@googlemail.com <sven.p.schliesing@googlemail.com@48955575-598b-d27f-7a68-2b204972dbf9>
2010-11-01 19:49:26 +0000

    juhu!

---
+++ WS2010_AD/src/a01/LinkedList.java
@@ -0,0 +1,157 @@
+package a01;
+
+import java.util.NoSuchElementException;
+
+public class LinkedList<T> implements Liste<T> {
+
+	private class Node {
+		private T data;
+		private Node next;
+		private Node prev;
+		
+		public Node(T data, Node next, Node prev) {
+			this.data = data;
+			this.next = next;
+			this.prev = prev;
+		}
+	}
+	
+	private Node head = new Node(null, null, null);
+	private Node tail = new Node(null, null, null);
+	private int size = 0;
+	
+	public LinkedList() {
+		head.next = tail;
+		tail.prev = head;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder buf = new StringBuilder();
+		buf.append("[");
+		Node currentNode = head.next;
+		while (currentNode.next != null) {
+			buf.append(currentNode.data);
+			buf.append(", ");
+			currentNode = currentNode.next;
+		}
+		buf.setLength(buf.length() - 2);
+		buf.append("]");
+		return buf.toString();
+	}
+	
+	@Override
+	public void add(int pos, T element) throws IndexOutOfBoundsException {
+		if (pos > size) {
+			throw new IndexOutOfBoundsException();
+		}
+		Node newNode = new Node(element, null, null);
+		Node currentNode = null;
+		if (pos <= size/2) {
+			currentNode = head.next;
+			for (int i = 0; i < pos; i++) {
+				currentNode = currentNode.next;
+			}			
+		} else {
+			currentNode = tail;
+			for (int i = size - 1 ; i >= pos; i--) {
+				currentNode = currentNode.prev;
+			}
+		}
+		// [head] <-> [0] <-> [1] -> [2] -> [tail]
+		// einfügen in pos: 1
+		// [head] <-> [0] <-> [new1] -> [1] -> [2] -> [tail]
+		newNode.prev = currentNode.prev;
+		newNode.next = currentNode;
+		currentNode.prev.next = newNode;
+		currentNode.prev = newNode;
+		size++;
+	}
+	
+	@Override
+	public void add(T element) {
+		add(size, element);
+		// [head] <-> [tail]
+		// einfügen in pos: $size
+		// [head] -> [0] -> [new1] -> [1] -> [2] -> [tail]
+//		Node newNode = new Node(element, null, null);
+//		newNode.prev = tail.prev;
+//		newNode.next = tail;
+//		tail.prev.next = newNode;
+//		tail.prev = newNode;
+//		size++;
+	}
+	
+	@Override
+	public T get(int pos) throws IndexOutOfBoundsException {
+		if (pos >= size) {
+			throw new IndexOutOfBoundsException();
+		}
+		
+		Node currentNode = getNodeOnPosition(pos);
+		return currentNode.data;
+	}
+
+	
+	@Override
+	public void remove(int pos) throws IndexOutOfBoundsException {
+		if (pos >= size) {
+			throw new IndexOutOfBoundsException();
+		}
+		
+		Node currentNode = getNodeOnPosition(pos);
+		currentNode.prev.next = currentNode.next;
+		currentNode.next.prev = currentNode.prev;
+		size--;
+	}
+	
+	private Node getNodeOnPosition(int pos) {
+		Node currentNode = null;
+		if (pos <= size/2) {
+			currentNode = head.next;
+			for (int i = 0; i < pos; i++) {
+				currentNode = currentNode.next;
+			}			
+		} else {
+			currentNode = tail;
+			for (int i = size; i > pos; i--) {
+				currentNode = currentNode.prev;
+			}
+		}
+		return currentNode;
+	}
+	
+	@Override
+	public void remove(T element) throws Exception {
+		boolean found = false;
+		Node currentNode = head.next;
+
+		while (currentNode != tail) {
+			if (currentNode.data == element) {
+				// [head] <-> [0] <-> [1] <-> [2] <-> [tail]
+				// entfernen von [1]
+				// [head] <-> [0] <-> [2] <-> [tail]
+				currentNode.prev.next = currentNode.next;
+				currentNode.next.prev = currentNode.prev;
+				size--;
+				found = true;
+			}
+			currentNode = currentNode.next;
+		}
+		
+		if (!found) {
+			throw new NoSuchElementException();
+		}
+		
+	}
+	
+	@Override
+	public boolean isEmpty() {
+		return size == 0;
+	}
+	
+	@Override
+	public int size() {
+		return size;
+	}
+}
---
+++ WS2010_AD/src/a02/ArrayList.java
@@ -0,0 +1,108 @@
+package a02;
+
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+import a01.Liste;
+
+public class ArrayList<T> implements Liste<T> {
+
+	public T[] elements;
+	private int size;
+
+	public ArrayList() {
+		this(10);
+	}
+
+	@SuppressWarnings("unchecked")
+	public ArrayList(int length) {
+		this.elements = (T[]) new Object[length];
+	}
+
+	@Override
+	public void add(int pos, T element) throws IndexOutOfBoundsException {
+		if (pos > this.size || pos < 0) {
+			throw new IndexOutOfBoundsException();
+		} else {
+			this.extendArray(this.size + 1);
+			for (int i = size; i > pos; i--) {
+				this.elements[i] = this.elements[i - 1];
+			}
+			this.elements[pos] = element;
+			this.size++;
+		}
+	}
+
+	@Override
+	public void add(T element) {
+		this.extendArray(this.size + 1);
+		this.elements[this.size] = element;
+		this.size++;
+	}
+
+	private void extendArray(int newsize) {
+		if (this.elements.length < newsize) {
+			this.elements = Arrays.copyOf(this.elements, newsize);
+		}
+	}
+
+	@Override
+	public T get(int pos) throws IndexOutOfBoundsException {
+		if (pos >= this.size) {
+			throw new IndexOutOfBoundsException();
+		}
+		return this.elements[pos];
+	}
+
+	@Override
+	public void remove(int pos) throws IndexOutOfBoundsException {
+		if (pos >= this.size || pos < 0) {
+			throw new IndexOutOfBoundsException();
+		} else {
+			for (int i = pos; i < this.size; i++) {
+				this.elements[i] = this.elements[i + 1];
+			}
+			this.elements[this.size] = null; // null setzen, freigabe für den
+			// gc.
+			this.size--;
+		}
+	}
+
+	@Override
+	public void remove(T element) throws Exception {
+		boolean found = false;
+		if (element == null) { // damit man nur einen vergleich hat. 
+			for (int i = 0; i < this.size;) {
+				if (this.elements[i] == null) {
+					this.remove(i);
+					found = true;
+				} else {
+					i++; // es wird nur erhöht, wenn wir keins entfent haben.
+				}
+			}
+		} else {
+			for (int i = 0; i < this.size;) {
+				if (element.equals(this.elements[i])) {
+					this.remove(i);
+					found = true;
+				} else {
+					i++;
+				}
+			}
+		}
+		if (!found) {
+			throw new NoSuchElementException();
+		}
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return this.size == 0;
+	}
+
+	@Override
+	public int size() {
+		return this.size;
+	}
+
+}
