34694e22e3fc6c7b0b65d7e73d12a49d8929b2d9
sven.p.schliesing@googlemail.com <sven.p.schliesing@googlemail.com@48955575-598b-d27f-7a68-2b204972dbf9>
2010-11-08 19:48:21 +0000

    blub

--- WS2010/src/a06ws/Elevator.java
+++ WS2010/src/a06ws/Elevator.java
@@ -1,15 +1,88 @@
 package a06ws;
 
-import javax.swing.JFrame;
 
-public class Elevator extends JFrame implements Runnable{
-
-
+public class Elevator implements Runnable {
+	
+	private int currentFloor;
+	private boolean busy;
+	private int entryFloor;
+	private int exitFloor;
+	
+	private boolean initialized;
+	private final int nr;
+	private ElevatorListener elevatorListener;
+	
+	public Elevator(int nr) {
+		this.nr = nr;
+		currentFloor = 0;
+		entryFloor = -1;
+		exitFloor = -1;
+		busy = false;
+		initialized = false;
+	}
 	
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		try {
+			synchronized (this) {
+				initialized = true;
+				this.wait();
+			}
+			busy = true;
+			
+			goToFloor(entryFloor);
+			System.out.println(nr + ": arrived at " + currentFloor);
+			
+			goToFloor(exitFloor);
+			System.out.println(nr + ": arrived at " + currentFloor);
+			busy = false;
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
+	private void goToFloor(int dstFloor) throws InterruptedException {
+		if (dstFloor != currentFloor) {
+			if (dstFloor > currentFloor) {
+				for (; currentFloor < dstFloor; currentFloor++) {
+					Thread.sleep(300);
+					elevatorListener.elevatorChangedFloor(nr, currentFloor, currentFloor - 1);
+					System.out.println(nr + ": " + currentFloor);
+				}
+			} else {
+				for (; currentFloor > dstFloor; currentFloor--) {
+					Thread.sleep(300);
+					elevatorListener.elevatorChangedFloor(nr, currentFloor, currentFloor + 1);
+					System.out.println(nr + ": " + currentFloor);
+				}
+			}
+		}
+	}
+	
+	public void goTo(int entryFloor, int exitFloor) {
+		this.entryFloor = entryFloor;
+		this.exitFloor = exitFloor;
+		synchronized (this) {
+			this.notify();
+		}
+	}
+	
+	public boolean isBusy() {
+		return busy;
+	}
+	
+	public int getCurrentFloor() {
+		return currentFloor;
+	}
+	
+	public boolean isInitialized() {
+		synchronized (this) {
+			return initialized;
+		}
+	}
+
+	public void setListener(ElevatorListener elevatorListener) {
+		this.elevatorListener = elevatorListener;
+	}
+	
 }
