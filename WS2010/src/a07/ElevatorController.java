d341c1775e61cb8dcf076d63c3a25152f0b740bd
sven.p.schliesing@googlemail.com <sven.p.schliesing@googlemail.com@48955575-598b-d27f-7a68-2b204972dbf9>
2010-11-15 20:16:43 +0000

    blub

--- WS2010/src/a07/ElevatorController.java
+++ WS2010/src/a07/ElevatorController.java
@@ -1,11 +1,10 @@
 package a07;
 
 import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 
 public class ElevatorController {
-	private ElevatorJob[] elevator;
+	private Elevator[] elevator;
 	
 	private ThreadPoolExecutor executor;
 	
@@ -23,18 +22,29 @@ public class ElevatorController {
 	
 	public ElevatorController(int elevatorCount, int floorCount) {
 		executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(elevatorCount);
-		
+		elevator = new Elevator[elevatorCount];
+        
+		for (int i = 0; i < elevator.length; i++) {
+			elevator[i] = new Elevator(i);
+			executor.execute(elevator[i]);
+		}	
 	}
 	
 	public void callElevator(int floor) {
-		ElevatorJob job = new ElevatorJob(floor);
-		executor.execute(job);
+		// eigentlich müsste man alle nicht-beschäftigten raussuchen und den FS nehmen der am dichtesten dran ist
+		Elevator chosen = null;
+		int length = 0;
+		for (int i = 0; i < elevator.length; i++) {
+			if (i == 0 || elevator[i].getQueueLength() < length) {
+				chosen = elevator[i];
+				length = elevator[i].getQueueLength();
+			}
+		}
+		chosen.goTo(floor);
 	}
 	
-	public void callPriorityElevator(int floor) {
-		ElevatorJob job = new ElevatorJob(floor);
-		((LinkedBlockingQueue<Runnable>) executor.getQueue()).clear();
-		executor.execute(job);
+	public void callElevatorPriority(int floor) {
+		elevator[0].goToPriority(floor);
 	}
 	
 	@Override
@@ -51,4 +61,18 @@ public class ElevatorController {
 			elevator[i].setListener(elevatorListener);
 		}
 	}
+
+	public void blockFahrstuhl(int fsNr) {
+		elevator[fsNr].block();
+	}
+
+	public void unblockFahrstuhl(int fsNr) {
+		elevator[fsNr].unblock();
+	}
+
+	public void driveTo(int fsNr, int floor) {
+		elevator[fsNr].driveTo(floor);
+	}
+
+
 }
