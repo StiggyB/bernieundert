d0d9ccf73af537b94aaa0a03c6c8c3a90431c5eb
sven.p.schliesing@googlemail.com <sven.p.schliesing@googlemail.com@48955575-598b-d27f-7a68-2b204972dbf9>
2010-12-30 17:55:16 +0000

    Ich möchte diesen Teppich nicht kaufen, bitte!

---
+++ WS2010/src/futtersuche/VehicleController.java
@@ -0,0 +1,29 @@
+package futtersuche;
+
+import java.util.HashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class VehicleController implements Runnable, FoodListener {
+
+	
+	private ExecutorService executor = Executors.newFixedThreadPool(100);
+	
+	public static void main(String[] args) {
+		Thread controllerThread = new Thread(new VehicleController());
+		controllerThread.start();
+	}		
+
+	@Override
+	public void run() {
+		for (int i = 0; i < 3; i++) {
+			executor.submit(new Vehicle(i, this));
+		}
+	}
+
+	@Override
+	public synchronized void foundFood(int type) {
+		System.out.println("Vehicle " + Thread.currentThread() + " found food: " + type);
+	}
+
+}
