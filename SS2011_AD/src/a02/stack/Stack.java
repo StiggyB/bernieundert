988d0ef51c6872ec699ebc9b04fcd523aa8d8eca
sven.p.schliesing@googlemail.com <sven.p.schliesing@googlemail.com@48955575-598b-d27f-7a68-2b204972dbf9>
2011-04-08 18:44:15 +0000

    huhu :)

---
+++ SS2011_AD/src/a02/stack/Stack.java
@@ -0,0 +1,35 @@
+package a02.stack;
+
+import java.util.LinkedList;
+
+public class Stack<T> implements IStack<T> {
+
+	LinkedList<T> list = new LinkedList<T>();
+	
+	
+	@Override
+	public void push(T element) {
+		list.add(element);
+	}
+
+	@Override
+	public void pop() {
+		list.removeLast();
+	}
+
+	@Override
+	public T top() {
+		return list.getLast();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return list.size() == 0;
+	}
+	
+	@Override
+	public String toString() {
+		return list.toString();
+	}
+
+}
