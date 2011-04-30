2190bc464b68b45229a64218ace8872b46c553b9
tell12345@web.de <tell12345@web.de@48955575-598b-d27f-7a68-2b204972dbf9>
2011-04-30 23:31:42 +0000

    a05

--- SS2011_AD_Last/src/a05/MainApp.java
+++ SS2011_AD_Last/src/a05/MainApp.java
@@ -3,44 +3,96 @@ package a05;
 public class MainApp {
 
 	final static int N = 20;
-	final static int M = 200;
+	final static int M = 20;
 	
 	public static void main(String[] args) {		
 		long time1;
 		long time2;
 		
-		System.out.println("\nPascals Triangle recursiv: \n");
+		// --------------------------------------------------------------
+		System.out.println("Pascals Triangle recursiv: ");
+		Benchmark.ops = 0;
 		time1 = System.currentTimeMillis();
+		
 		for (int i = 0; i < M; i++) {
-			for (int j = 1; j <= N; j++) {
-				System.out.print(PascalsTriangle.calcTriangleRecu(N+1, j) + "  ");
-			}
-			System.out.println();
+			Benchmark.ops = 0;
+//				System.out.print(PascalsTriangle.calcTriangleRecu(N+1, j) + "  ");
+				PascalsTriangle.calcTriangleRecu(i);
+			System.out.print(Benchmark.ops + " ");
 		}	
-		time2 = System.currentTimeMillis();
 		System.out.println();
-		System.out.println("Zeit: " + (time2 - time1));
+		for (int i = 0; i <= M; i++) {
+			System.out.print(i + " ");
+		}
+		time2 = System.currentTimeMillis();
+		System.out.println("\nZeit: " + (time2 - time1));
 		
+		// --------------------------------------------------------------
 		System.out.println("\n\nPascals Triangle iterativ: ");
+		Benchmark.ops = 0;
 		time1 = System.currentTimeMillis();
 		for (int i = 0; i < M; i++) {
-			PascalsTriangle.printArr(PascalsTriangle.calcTriangleIter(N), N);
-			System.out.println();
+			Benchmark.ops = 0;
+//			PascalsTriangle.printArr(PascalsTriangle.calcTriangleIter(N), N);
+			PascalsTriangle.calcTriangleIter(i);
+			System.out.print(Benchmark.ops + " ");
 		}
 		time2 = System.currentTimeMillis();
-		System.out.println();
-		System.out.println("Zeit: " + (time2 - time1));
+		System.out.println("\nZeit: " + (time2 - time1));
 		
-		System.out.println("\n\nPascals Triangle binomial: ");
+		// --------------------------------------------------------------
+		System.out.println("\n\nPascals Triangle iterativ faster: ");
+		Benchmark.ops = 0;
 		time1 = System.currentTimeMillis();
 		for (int i = 0; i < M; i++) {
-			for (int j = 0; j <= N; j++) {
-				System.out.print(PascalsTriangle.calcBinomial(N, j) + "  ");
-			}
-			System.out.println();
+			Benchmark.ops = 0;
+//			PascalsTriangle.pascalIterativeSlow(N);
+			PascalsTriangle.pascalIterativeFaster(i);
+			System.out.print(Benchmark.ops + " ");
+			
 		}
 		time2 = System.currentTimeMillis();
-		System.out.println();
-		System.out.println("Zeit: " + (time2 - time1));
+		System.out.println("\nZeit: " + (time2 - time1));
+		
+		// --------------------------------------------------------------
+		System.out.println("\n\nPascals Triangle binomial Tugend: ");
+		Benchmark.ops = 0;
+		time1 = System.currentTimeMillis();
+		for (int i = 0; i <= M; i++) {
+			Benchmark.ops = 0;
+//			PascalsTriangle.pascalBinomialMax20(N);
+			PascalsTriangle.pascalBinomialGreaterThan20(i);
+			System.out.print(Benchmark.ops + " ");
+		}
+		time2 = System.currentTimeMillis();
+		System.out.println("\nZeit: " + (time2 - time1));
+		
+		// --------------------------------------------------------------
+		System.out.println("\n\nPascals Triangle binomial: ");
+		Benchmark.ops = 0;
+		time1 = System.currentTimeMillis();
+		for (int i = 0; i < M; i++) {
+			Benchmark.ops = 0;
+			for (int j = 0; j <= i; j++) {
+//				System.out.print(PascalsTriangle.calcBinomial(N, j) + "  ");
+				PascalsTriangle.calcBinomial(i, j);	
+			}
+			System.out.print(Benchmark.ops + " ");
+		}
+		time2 = System.currentTimeMillis();
+		System.out.println("\nZeit: " + (time2 - time1));
+		
+		// --------------------------------------------------------------
+		System.out.println("\n\nPascals Triangle special binomial: ");
+		Benchmark.ops = 0;
+		time1 = System.currentTimeMillis();
+		for (int i = 0; i < M; i++) {
+			Benchmark.ops = 0;
+//			PascalsTriangle.pascalSpecial(N);
+			PascalsTriangle.pascalSpecial(i);
+			System.out.print(Benchmark.ops + " ");
+		}
+		time2 = System.currentTimeMillis();
+		System.out.println("\nZeit: " + (time2 - time1));
 	}
 }
--- SS2011_AD_Last/src/a05/PascalsTriangle.java
+++ SS2011_AD_Last/src/a05/PascalsTriangle.java
@@ -1,7 +1,17 @@
 package a05;
 
 public class PascalsTriangle {
-	
+
+	/**
+	 * @param n
+	 */
+	static void calcTriangleRecu(int n) {
+		n++;
+		for (int i = 1; i < n; i++) {
+			calcTriangleRecu(n, i);
+		}
+	}
+
 	/**
 	 * P(n, k) = P(n-1, k-1) + P(n-1, k)
 	 * 
@@ -10,37 +20,66 @@ public class PascalsTriangle {
 	 * @return
 	 */
 	static int calcTriangleRecu(int n, int k) {
-//		System.out.println("\nVales: " + n + ", " + k);
+		Benchmark.ops++;
 		if (n < k || k < 0) {
 			return 0;
 		}
 		if (k == 1 || k == n) {
-			return 1;		
+			return 1;
 		}
-		return calcTriangleRecu(n-1, k-1) + calcTriangleRecu(n-1, k);
+		return calcTriangleRecu(n - 1, k - 1) + calcTriangleRecu(n - 1, k);
 	}
-	
+
 	/**
 	 * P(n, k) = P(n-1, k-1) + P(n-1, k)
 	 * 
 	 */
 	static long[][] calcTriangleIter(int m) {
-		long [][] values = new long[m+1][m+1];
+		long[][] values = new long[m + 1][m + 1];
 		for (int n = 0; n < values.length; n++) {
 			for (int k = 0; k <= n; k++) {
-				if(k == 0 || k == n) {
+				Benchmark.ops++;
+				if (k == 0 || k == n) {
 					values[n][k] = 1;
 				} else {
-					values[n][k] = values[n-1][k-1] + values[n-1][k];
+					values[n][k] = values[n - 1][k - 1] + values[n - 1][k];
 				}
 			}
 		}
 		return values;
 	}
-	
+
 	/**
-	 * Only for n >= k and n nonnegative
-	 * (n,k) = n! / k! * (n-k)!
+	 * Von der Tugend
+	 * 
+	 * @param n
+	 */
+	public static void pascalIterativeFaster(int n) {
+		long triangle[][] = new long[n + 1][];
+
+		for (int i = 0; i <= n; i++) {
+			triangle[i] = new long[i + 1];
+
+			for (int j = 0; j <= i; j++) {
+				Benchmark.ops++;
+				if ((j == 0) || (j == i))
+					triangle[i][j] = 1;
+				else
+					triangle[i][j] = triangle[i - 1][j - 1]
+							+ triangle[i - 1][j];
+
+				// System.out.print(" " + triangle[i][j]);
+			}
+			// System.out.println();
+		}
+		// for (int i = 0; i < triangle.length; i++) {
+		// System.out.print(triangle[n][i] + " ");
+		// ;
+		// }
+	}
+
+	/**
+	 * Only for n >= k and n nonnegative (n,k) = n! / k! * (n-k)!
 	 * 
 	 * @param n
 	 * @param k
@@ -48,24 +87,40 @@ public class PascalsTriangle {
 	 */
 	static long calcBinomial(int n, int k) {
 		long res = 0;
-		if(n >= k || n >= 0) {
-			res = (factor(n)) / (factor(k) * factor((n-k)));
+		if (n >= k || n >= 0) {
+			Benchmark.ops++;
+			res = (Factorial.factor(n))
+					/ (Factorial.factor(k) * Factorial.factor((n - k)));
 		}
 		return res;
 	}
 	
-	/**
-	 * @param n
-	 * @return
-	 */
-	static long factor(int n) {
-		long res = 1;
-		for (int i = 1; i <= n; i++) {
-			res *= i;
+	// geht bis n=136
+	public static void pascalBinomialGreaterThan20(int n) {
+		for (int i = 0; i <= n; i++) {
+			System.out.print(Factorial.factorialGreaterThan20recursive(n)
+					.divide(Factorial.factorialGreaterThan20recursive(i)
+							.multiply(
+									Factorial.factorialGreaterThan20recursive(n
+											- i)))
+					+ " ");
 		}
-		return res;
+
 	}
-	
+
+	// n = 60 ca.
+	public static void pascalSpecial(int n) {
+		int j = 0;
+		long x = 1;
+		for (j = 0, x = 1; j <= n; j++) {
+			// System.out.print(x + " ");
+			x = x * (n - j) / (j + 1);
+			Benchmark.ops++;
+		}
+		// System.out.println();
+
+	}
+
 	static void printArr(long[][] arr) {
 		for (int i = 0; i < arr.length; i++) {
 			System.out.println();
@@ -74,11 +129,11 @@ public class PascalsTriangle {
 			}
 		}
 	}
-	
+
 	static void printArr(long[][] arr, int n) {
 		for (int i = 0; i < arr.length; i++) {
 			System.out.print(arr[n][i] + "  ");
 		}
 	}
-	
+
 }
