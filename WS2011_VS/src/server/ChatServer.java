2dc1c2c9d0fe9b9d5a5169ce513b2e005f4ea354
tell12345@web.de <tell12345@web.de@48955575-598b-d27f-7a68-2b204972dbf9>
2011-10-14 11:50:22 +0000

    comm with various clients impl remember time impl

--- WS2011_VS/src/server/ChatServer.java
+++ WS2011_VS/src/server/ChatServer.java
@@ -6,18 +6,13 @@ public class ChatServer {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
 		System.out.println("Setting up server...");
-		
 		try {
 			ChatServerImpl impl = new ChatServerImpl();
 			impl.setUp();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
 		System.out.println("Server up an running...");
-		
 	}
-
 }
