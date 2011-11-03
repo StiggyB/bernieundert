2c8cc1c9389a403178e4241650de9963532caffd
tell12345@web.de <tell12345@web.de@48955575-598b-d27f-7a68-2b204972dbf9>
2011-11-03 08:34:08 +0000

    calculation of pollard impl

--- Worker/src/a02/Calculator.java
+++ Worker/src/a02/Calculator.java
@@ -4,65 +4,112 @@ import java.math.BigInteger;
 import java.util.Random;
 
 public class Calculator implements Runnable {
-	private boolean running = false;
+	
+	public static final int MAX_ITERATIONS = 1000;
+	
+	private boolean found = false;
 	private Worker worker;
 	private BigInteger n;
-	private BigInteger a;
+	private BigInteger a = new BigInteger(new Random().nextInt(1000), new Random());
+	private BigInteger prime;
+	private BigInteger factor;
 	private BigInteger result;
-	
-	public BigInteger getResult() {
-		return result;
-	}
 
-	public Calculator(Worker worker, BigInteger n, BigInteger a) {
+	public Calculator(Worker worker, BigInteger n) {
 		this.worker = worker;
 		this.n = n;
-		this.a = a;
-		this.result = null;
+		this.prime = n;
+		this.factor = null;
+		this.result = BigInteger.ONE;
 	}
 	
 	@Override
 	public void run() {
-		this.worker.getFactorList();
-		while(!running) {
-			result = pollard(n, a);
-			if(result == null) {
-				running = true;
-			}
-		}
+		
+//		 do{
+//			   b = b.divide(sl.factor);
+//			   sl = PollardMethod(b,BigInteger.valueOf(3));
+//			   System.out.println(sl.toString());
+//			   System.out.println(isPrime(sl.factor));
+//			  }while(b != sl.factor);
+		
+			do {
+				prime = prime.divide(result);
+				result = pollard(prime);
+				System.out.println(result);
+			} while (prime != result && result != null);
+			System.out.println(prime);
+		
+		
+//		for(int i = 0; i < MAX_ITERATIONS && !(found); i++) {
+//			factor = pollard(n);
+//			System.out.println("Factor: " + factor);
+//			if (factor == null) {
+//				found = true;
+//			} else {
+//				if(this.worker.getFactorList().contains(factor)) {
+//					if(isCompletePrim()) {
+//						found = true;
+//					} else {
+//						a = new BigInteger(new Random().nextInt(1000), new Random());
+//					}
+//				} else if (isPrime(factor)){ // TODO Without isPrime more efficient
+//					worker.add(factor);
+//				}
+//			}
+//		}
+//		this.worker.pollardFinished();
 	}
 
-	private BigInteger pollard(BigInteger n, BigInteger a) {
-		BigInteger zero = new BigInteger("0");
-		BigInteger one = new BigInteger("1");
+	// TODO Accept duplicated factors! (e.i. 29 * 29)
+	// Maybe more than the searched factors in the factorList...
+	private boolean isCompletePrim() {
+		boolean bool = false;
 		
-		if (n.mod(new BigInteger("2")).equals(zero)) {
+		for (BigInteger factor : this.worker.getFactorList()) {
+			result = result.multiply(factor); 
+		}
+		if(result.equals(prime)) {
+			System.out.println("COMPLETE!");
+			bool = true;
+		}
+		return bool;
+	}
+	
+	private boolean isPrime(BigInteger factor) {
+		for (BigInteger i = new BigInteger("2"); factor.compareTo(i) > 0; i = i.add(BigInteger.ONE)) {
+			if(factor.mod(i).equals(BigInteger.ZERO)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private BigInteger pollard(BigInteger n) {
+		
+		if (n.mod(new BigInteger("2")).equals(BigInteger.ZERO)) {
 			return null;
 		}
 		
 		Random rand = new Random();
-		BigInteger x = new BigInteger(n.bitCount(), rand);
-		System.out.println("Radnom value: " + x);
+		BigInteger x;
+		do {
+			x = new BigInteger(n.bitCount(), rand);
+		} while (x.compareTo(n) > 0);
 		BigInteger y = x;
-		BigInteger p = one;
-		BigInteger d = zero;
+		BigInteger p = BigInteger.ONE;
+		BigInteger d = BigInteger.ZERO;
 		do {
 			x = (x.multiply(x).add(a).mod(n));
 			y = (y.multiply(y).add(a).mod(n));
 			y = (y.multiply(y).add(a).mod(n));
 			d = (y.subtract(x).mod(n));
-			p = ggt(d, n);
-		} while (p.equals(one));
+			p = n.gcd(d);
+		} while (p.equals(BigInteger.ONE));
 
 		if (!(p.equals(n))) {
 			return p;
 		}
 		return null;
 	}
-
-	private BigInteger ggt(BigInteger d, BigInteger n) {
-		BigInteger bi = new BigInteger(("" + d));
-		return bi.gcd(new BigInteger("" + n));
-	}
-
 }
--- Worker/src/a02/Worker.java
+++ Worker/src/a02/Worker.java
@@ -16,9 +16,6 @@ public class Worker extends UntypedActor {
 	private ActorRef master;
 	private List<BigInteger> factorList = new ArrayList<BigInteger>();
 
-	public List<BigInteger> getFactorList() {
-		return factorList;
-	}
 
 	public Worker() {
 		// Wichtig: Wenn die ID nicht gesetzt wird, wird immer dieselbe In-
@@ -29,48 +26,50 @@ public class Worker extends UntypedActor {
 		idGenerator++;
 	}
 	
+	public List<BigInteger> getFactorList() {
+		return factorList;
+	}
+	
 	@Override
 	public void onReceive(Object message) {
-		BigInteger result = null;
+		List<BigInteger> result = null;
 		if (message instanceof CalculateMessage) {
 			// Beim ersten Aufruf wird der Sender ermittel
 			this.master = getContext().getSender().get();
 			CalculateMessage calculateMessage = (CalculateMessage) message;
-			Calculator calc = new Calculator(this, calculateMessage.getA(),
-					calculateMessage.getB());
+			Calculator calc = new Calculator(this, calculateMessage.getN());
 			calc.run();
-			result = calc.getResult();
-			if (result == null) {
-				// calculate one more time
-			} else if (factorList.contains(result))  {
-				// find next factor of this product
-			}
-//			result = calculate(calculateMessage.getA(),
-//					calculateMessage.getB());
-			if (WorkerInfo.getAcRefList().contains(result)) {
-				
-			}
-			ResultMessage resultMessage = new ResultMessage(result);
-			System.out.println("TELL");
-			// Ergebnis an den Master senden
-			master.tell(resultMessage);
-			for (ActorRef ar : WorkerInfo.getAcRefList()) {
-				ar.tell(resultMessage);
-			}
-			// Durch this.getContext().tell([Nachricht]) kann der Aktor
-			// sich selbst eine Nachricht schicken. In diesem Fall schickt
-			// sich der Aktor eine "poisonPill". Empfängt ein Aktor diese,
-			// beendet er sich und postStop() wird aufgerufen.
-			getContext().tell(poisonPill());
 		} else if (message instanceof ResultMessage) {
-			result = ((ResultMessage) message).getResult();
+			result = ((ResultMessage) message).getResults();
 			if(!(factorList.contains(result))) {
-				factorList.add(result);
+				factorList.addAll(result);
 			}
 		} else {
 			throw new IllegalArgumentException("Unknown message [" + message
 					+ "]");
 		}
+	}
+	
+	public void add(BigInteger factor) {
+		if(factor != null) {
+			this.factorList.add(factor);
+		} else {
+			throw new NullPointerException();
+		}
+	}
+	
+	public void pollardFinished() {
+		if (factorList.isEmpty()) {
+			System.out.println("NUMBER IS NO PRIME!");
+		} else {
+			ResultMessage resultMessage = new ResultMessage(factorList);
+			master.tell(resultMessage);
+		}
+		// Durch this.getContext().tell([Nachricht]) kann der Aktor
+		// sich selbst eine Nachricht schicken. In diesem Fall schickt
+		// sich der Aktor eine "poisonPill". Empfängt ein Aktor diese,
+		// beendet er sich und postStop() wird aufgerufen.
+		getContext().tell(poisonPill());
 	}
 
 	@Override
@@ -79,11 +78,11 @@ public class Worker extends UntypedActor {
 		super.postStop();
 	}
 
-	public void start() {
-		remote().start("localhost", 2554);
+	public static void start() {
+		remote().start("localhost", 2551);
 	}
 	
 	public static void main(String[] args) {
-		remote().start("localhost", 2554);
+		remote().start("localhost", 2552);
 	}
 }
