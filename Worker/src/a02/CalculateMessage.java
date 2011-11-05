f0a921f5b110232f7e935ff8192c3fa697bc5838
tell12345@web.de <tell12345@web.de@48955575-598b-d27f-7a68-2b204972dbf9>
2011-11-05 18:40:03 +0000

    worker full impl

--- Worker/src/a02/CalculateMessage.java
+++ Worker/src/a02/CalculateMessage.java
@@ -7,24 +7,18 @@ public class CalculateMessage implements Serializable {
 
 	private static final long serialVersionUID = 840244832287440949L; 
 	private BigInteger n;
-	private int nThreads;
 	
-	public int getnThreads() {
-		return nThreads;
-	}
 
-	public CalculateMessage(BigInteger n, int nThreads) { 
+	public CalculateMessage(BigInteger n) { 
 		this.n = n;
-		this.nThreads = nThreads;
 	}
 	
 	public BigInteger getN() { 
 		return n;
 	}
-
+	
 	@Override
 	public String toString() {
-		return "CalculateMessage [n=" + n + ", nThreads=" + nThreads + "]";
+		return "CalculateMessage [n=" + n + "]";
 	}
-	
 }
