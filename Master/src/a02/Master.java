7bc9a1a5a00e96bd384cd0849190a39072775a9b
tell12345@web.de <tell12345@web.de@48955575-598b-d27f-7a68-2b204972dbf9>
2011-11-05 18:39:42 +0000

    shutdown all worker impl

--- Master/src/a02/Master.java
+++ Master/src/a02/Master.java
@@ -1,11 +1,12 @@
 package a02;
 
-import static akka.actor.Actors.poisonPill;
 import static akka.actor.Actors.remote;
 
 import java.math.BigInteger;
+import java.util.List;
 
 import akka.actor.ActorRef;
+import akka.actor.Actors;
 import akka.actor.UntypedActor;
 import akka.remoteinterface.RemoteServerModule;
 
@@ -23,22 +24,23 @@ public class Master extends UntypedActor {
 		System.out.println("RECEIVE");
 		if (message instanceof CalculateMessage) {
 	           CalculateMessage calculate = (CalculateMessage) message;
-	           startNWorkers(1);
+	           startNWorkers(4);
 	           ActorRef me = getContext();
 	           for (ActorRef worker : WorkerInfo.getWorkerRefs()) {
 	        	   System.out.println("TELL");
 	        	   worker.tell(calculate, me);
 	           }
-		} else if (message instanceof ResultMessage) { 
-			if (((ResultMessage) message).getResults() == null) {
+		} else if (message instanceof ResultMessage) {
+			for (ActorRef actor : WorkerInfo.getWorkerRefs()) {
+				actor.stop();
+			}
+			Actors.registry().shutdownAll();
+			List<BigInteger> results = ((ResultMessage) message).getResults();
+			if (results == null) {
 				System.out.println("NO PRIME!");
 			} else {
-				for (BigInteger factor : ((ResultMessage) message).getResults()) {
-					System.out.println("PRIME(S): " + factor);
-				}
-				System.out.println("PRIME(S): " + ((ResultMessage) message).getResults());
+				System.out.println("PRIME(S): " + results);
 			}
-			getContext().tell(poisonPill());
 		} else {
 			throw new IllegalArgumentException("Unknown message [" +
 	                             message + "]");
@@ -47,8 +49,9 @@ public class Master extends UntypedActor {
 	
 	public void startNWorkers(int nWorkers) {
 		for (int iPort = DEFAULT_PORT; iPort < (nWorkers + DEFAULT_PORT); iPort++) {
-			ActorRef worker = remote().actorFor(Worker.class.getName(), "localhost", iPort);
-			 WorkerInfo.getWorkerRefs().add(worker);
+			System.out.println("WORKER: " + iPort);
+			ActorRef worker = remote().actorFor(Worker.class.getName(), "localhost", DEFAULT_PORT);
+			WorkerInfo.getWorkerRefs().add(worker);
 		}
 	}
 
@@ -56,9 +59,8 @@ public class Master extends UntypedActor {
 			remoteSupport = remote().start("localhost", 2553);
 			ActorRef client = remote().actorFor(Master.class.getName(),
 					"localhost", 2553);
-			BigInteger n = new BigInteger("8806715679");
-			int nThreads = 2;
-			CalculateMessage calculate = new CalculateMessage(n, nThreads);
+			BigInteger n = new BigInteger("9398726230209357241");
+			CalculateMessage calculate = new CalculateMessage(n);
 			client.tell(calculate);
 	}
 }
