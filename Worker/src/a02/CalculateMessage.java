09cd1abd859d65a9eda944053ce1a54580ab2038
tell12345@web.de <tell12345@web.de@48955575-598b-d27f-7a68-2b204972dbf9>
2011-11-05 09:46:57 +0000

    full calc handling

--- Worker/src/a02/CalculateMessage.java
+++ Worker/src/a02/CalculateMessage.java
@@ -13,9 +13,9 @@ public class CalculateMessage implements Serializable {
 		return nThreads;
 	}
 
-	public CalculateMessage(BigInteger n) {// int nThreads) { 
+	public CalculateMessage(BigInteger n, int nThreads) { 
 		this.n = n;
-//		this.nThreads = nThreads;
+		this.nThreads = nThreads;
 	}
 	
 	public BigInteger getN() { 
