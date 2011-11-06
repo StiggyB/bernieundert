8fdf6e96c3e418bb32ebd349c1ccffe27871709d
tell12345@web.de <tell12345@web.de@48955575-598b-d27f-7a68-2b204972dbf9>
2011-11-06 11:59:44 +0000

    full impl waiting for gui

--- Worker/src/a02/Calculator.java
+++ Worker/src/a02/Calculator.java
@@ -4,12 +4,11 @@ import java.math.BigInteger;
 import java.util.Random;
 
 public class Calculator implements Runnable {
-	
-	public static final int MAX_ITERATIONS = 10;
-	
+
 	private boolean isRunning;
 	private Worker worker;
-	private BigInteger a = new BigInteger(new Random().nextInt(1000), new Random());
+	private BigInteger a = new BigInteger(new Random().nextInt(1000),
+			new Random());
 	private BigInteger prime;
 
 	private BigInteger result;
@@ -20,77 +19,81 @@ public class Calculator implements Runnable {
 		this.prime = n;
 		this.result = BigInteger.ONE;
 	}
-	
+
 	public boolean isRunning() {
 		return isRunning;
 	}
-	
+
 	public void setRunning(boolean isRunning) {
 		this.isRunning = isRunning;
 	}
-	
+
 	public BigInteger getPrime() {
 		return prime;
 	}
-	
+
 	@Override
 	public void run() {
 		long startTime = System.currentTimeMillis();
-		for (int i = 0; i < MAX_ITERATIONS && isRunning; i++) {
-			System.out.println("CALC!");
+		for (int i = 0; i < worker.getMaxIterations() && isRunning; i++) {
 			result = pollard(prime);
 			if (result == null) {
 				isRunning = false;
 				if (!worker.getFactorList().isEmpty()) {
-					worker.pollardFinished(prime, System.currentTimeMillis() - startTime);
+					worker.pollardFinished(prime, System.currentTimeMillis()
+							- startTime);
 				} else {
-					worker.pollardFinished(result, System.currentTimeMillis() - startTime);
+					worker.pollardFinished(result, System.currentTimeMillis()
+							- startTime);
+				}
+			} else if (isPrime(result)) {
+				isRunning = false;
+				worker.pollardFinished(result, System.currentTimeMillis()
+						- startTime);
+			}
+		}
+	}
+
+	synchronized public static boolean isPrime(BigInteger number) {
+		BigInteger two = new BigInteger("2");
+		if (number.compareTo(two) < 0) {
+			return false;
+		} else if (number.equals(two)) {
+			return true;
+		} else {
+			if (number.mod(two).equals(BigInteger.ZERO)) {
+				return false;
+			}
+			BigInteger sqrt = sqrt(number);
+			for (BigInteger i = new BigInteger("3"); i.compareTo(sqrt) <= 0; i = i
+					.add(two)) {
+				if (number.mod(i).equals(BigInteger.ZERO)) {
+					return false;
 				}
 			}
-			else if(isPrime(result)) {
-				isRunning = false;
-				worker.pollardFinished(result, System.currentTimeMillis() - startTime);
-			}
-		}	
-		System.out.println("JOIN IT!");
+			return true;
+		}
 	}
-	
-	synchronized public static boolean isPrime(BigInteger number) {
-        BigInteger two = new BigInteger("2");
-        if (number.compareTo(two) < 0) {
-            return false;
-        } else if (number.equals(two) ) {
-            return true;
-        } else {
-            if (number.mod(two).equals(BigInteger.ZERO)) {
-                return false;
-            }
-            BigInteger sqrt = sqrt(number);
-            for (BigInteger i = new BigInteger("3"); i.compareTo(sqrt) <= 0; i = i.add(two)) {
-                if (number.mod(i).equals(BigInteger.ZERO)) {
-                    return false;
-                }
-            }
-            return true;
-        }
-    }
 
 	synchronized private static BigInteger sqrt(BigInteger n) {
-        BigInteger a = BigInteger.ONE;
-        BigInteger b = new BigInteger(n.shiftRight(5).add(new BigInteger("8")).toString());
-        while(b.compareTo(a) >= 0) {
-          BigInteger mid = new BigInteger(a.add(b).shiftRight(1).toString());
-          if(mid.multiply(mid).compareTo(n) > 0) b = mid.subtract(BigInteger.ONE);
-          else a = mid.add(BigInteger.ONE);
-        }
-        return a.subtract(BigInteger.ONE);
-      }
+		BigInteger a = BigInteger.ONE;
+		BigInteger b = new BigInteger(n.shiftRight(5).add(new BigInteger("8"))
+				.toString());
+		while (b.compareTo(a) >= 0) {
+			BigInteger mid = new BigInteger(a.add(b).shiftRight(1).toString());
+			if (mid.multiply(mid).compareTo(n) > 0)
+				b = mid.subtract(BigInteger.ONE);
+			else
+				a = mid.add(BigInteger.ONE);
+		}
+		return a.subtract(BigInteger.ONE);
+	}
 
 	synchronized private BigInteger pollard(BigInteger n) {
 		if (n.mod(new BigInteger("2")).equals(BigInteger.ZERO)) {
 			return null;
 		}
-		
+
 		Random rand = new Random();
 		BigInteger x;
 		do {
